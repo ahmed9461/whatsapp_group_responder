5bc36810aca111fe0715605c44bf34a2a6d299f6
ahmed9461 <128654158+ahmed9461@users.noreply.github.com>
2026-08-12 17:09:47 -0700

    fix(app): reconnect cleanly after resume

--- lib/app.dart
+++ lib/app.dart
@@ -1,32 +1,59 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
 import 'core/app_controller.dart';
 import 'core/theme.dart';
 import 'features/enrollment/enrollment_page.dart';
 import 'features/shell/home_shell.dart';
 
-class ResponderApp extends StatelessWidget {
+class ResponderApp extends StatefulWidget {
   const ResponderApp({super.key, required this.controller});
 
   final AppController controller;
 
   @override
+  State<ResponderApp> createState() => _ResponderAppState();
+}
+
+class _ResponderAppState extends State<ResponderApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      unawaited(widget.controller.handleAppResumed());
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: controller,
+      animation: widget.controller,
       builder: (context, _) {
         return MaterialApp(
           title: 'ردود واتساب',
           debugShowCheckedModeBanner: false,
           theme: buildAppTheme(Brightness.light),
           darkTheme: buildAppTheme(Brightness.dark),
-          themeMode: controller.themeMode,
+          themeMode: widget.controller.themeMode,
           builder: (context, child) => Directionality(
             textDirection: TextDirection.rtl,
             child: child ?? const SizedBox.shrink(),
           ),
-          home: controller.isLinked
-              ? HomeShell(controller: controller)
-              : EnrollmentPage(controller: controller),
+          home: widget.controller.isLinked
+              ? HomeShell(controller: widget.controller)
+              : EnrollmentPage(controller: widget.controller),
         );
       },
     );
